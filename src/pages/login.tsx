f4770723748dc1210489cfb8ba8d47358d360f9c
ejiro1994 <ejiro1994@gmail.com>
2023-03-06 01:30:00 +0000

    auth working

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,71 +1,72 @@
-import { useEffect, useRef } from "react";
-import { useLogin } from "@pankod/refine-core";
-import { Container, Box } from "@pankod/refine-mui";
+import { useEffect, useRef } from 'react'
+import { useLogin } from '@pankod/refine-core'
+import { Container, Box } from '@pankod/refine-mui'
 
-import { CredentialResponse } from "../interfaces/google";
+import { yariga } from '../assets'
+
+import { CredentialResponse } from '../interfaces/google'
 
 export const Login: React.FC = () => {
-  const { mutate: login } = useLogin<CredentialResponse>();
+  const { mutate: login } = useLogin<CredentialResponse>()
 
   const GoogleButton = (): JSX.Element => {
-    const divRef = useRef<HTMLDivElement>(null);
+    const divRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-      if (typeof window === "undefined" || !window.google || !divRef.current) {
-        return;
+      if (typeof window === 'undefined' || !window.google || !divRef.current) {
+        return
       }
 
       try {
         window.google.accounts.id.initialize({
-          ux_mode: "popup",
-          client_id: "your-client-id",
+          ux_mode: 'popup',
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
-              login(res);
+              login(res)
             }
           },
-        });
+        })
         window.google.accounts.id.renderButton(divRef.current, {
-          theme: "filled_blue",
-          size: "medium",
-          type: "standard",
-        });
+          theme: 'outline',
+          size: 'large',
+          type: 'standard',
+        })
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
-    }, []); // you can also add your client id as dependency here
+    }, []) // you can also add your client id as dependency here
 
-    return <div ref={divRef} />;
-  };
+    return <div ref={divRef} />
+  }
 
   return (
     <Box
-      component="div"
+      component='div'
       sx={{
-        background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
-        backgroundSize: "cover",
+        backgroundColor: `#FCFCFC`,
       }}
     >
       <Container
-        component="main"
-        maxWidth="xs"
+        component='main'
+        maxWidth='xs'
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          height: "100vh",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '100vh',
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
           <div>
-            <img src="./refine.svg" alt="Refine Logo" />
+            <img src={yariga} alt='Refine Logo' />
           </div>
           <Box mt={4}>
             <GoogleButton />
@@ -73,5 +74,5 @@ export const Login: React.FC = () => {
         </Box>
       </Container>
     </Box>
-  );
-};
+  )
+}
